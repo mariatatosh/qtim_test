a07cba8c15e43e76ae6f7793e950509a5afdf2fd
Maria Tatosh <tatoshmaria@protonmail.com>
2023-01-22 19:59:53 +0300

    Add "has by email" method for user

--- api/src/Repository/UserRepository.php
+++ api/src/Repository/UserRepository.php
@@ -2,9 +2,12 @@
 
 namespace App\Repository;
 
+use App\Entity\Types\Email\Email;
+use App\Entity\Types\Id\Id;
 use App\Entity\User\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use DomainException;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
@@ -27,12 +30,40 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
      */
     public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
     {
-        if (!$user instanceof User) {
+        if (! $user instanceof User) {
             throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
         }
 
         $user->setPassword($newHashedPassword);
 
         $this->save($user);
+    }
+
+    /**
+     * @param \App\Entity\Types\Email\Email $email
+     *
+     * @return bool
+     */
+    public function hasByEmail(Email $email): bool
+    {
+        return count(
+                $this->getEntityManager()->getRepository(User::class)->findBy(['email' => $email->getValue()])
+            ) > 0;
+    }
+
+    /**
+     * @param \App\Entity\Types\Id\Id $id
+     *
+     * @return \App\Entity\User\User
+     */
+    public function get(Id $id): User
+    {
+        $user = $this->find($id->getValue());
+
+        if ($user instanceof User) {
+            return $user;
+        }
+
+        throw new DomainException('User not found');
     }
 }
