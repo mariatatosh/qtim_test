ef46cd535b3513b92a20914f400734fbdb3a3c55
Maria Tatosh <tatoshmaria@protonmail.com>
2023-01-24 20:08:26 +0300

    Setup lib for editing text

---
+++ frontend/src/components/TextEditor.vue
@@ -0,0 +1,37 @@
+<template>
+  <ckeditor v-model="input" :editor="editor" />
+</template>
+
+<script>
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+export default {
+  name: 'TextEditor',
+  props: {
+    modelValue: {
+      type: String,
+      required: true,
+    },
+  },
+  data: () => ({
+    editor: ClassicEditor,
+    data: '',
+    input: '',
+  }),
+  watch: {
+    input(newVal) {
+      this.$emit('update:modelValue', newVal);
+    },
+  },
+};
+</script>
+
+<style>
+.ck-editor {
+  width: 100% !important;
+}
+
+.ck-editor__editable {
+  min-height: 300px;
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue';
+import CKEditor from '@ckeditor/ckeditor5-vue';
+
 import App from './App.vue';
 
 import vuetify from './plugins/vuetify';
@@ -7,4 +9,5 @@ import router from './plugins/router';
 createApp(App)
     .use(vuetify)
     .use(router)
+    .use(CKEditor)
     .mount('#app');
